a6187fe7caa4384e73196804962f6aeb7d3af1a8
Jonathon Marolf <jmarolf@microsoft.com>
2020-11-10 08:29:43 -0800

    don't use syntax editor

--- BraceAnalyzer/BraceAnalyzer.CodeFixes/BraceAnalyzerCodeFixProvider.cs
+++ BraceAnalyzer/BraceAnalyzer.CodeFixes/BraceAnalyzerCodeFixProvider.cs
@@ -1,20 +1,16 @@
 ﻿namespace BraceAnalyzer
 {
+    using System.Collections.Immutable;
+    using System.Composition;
+    using System.Linq;
+    using System.Threading.Tasks;
+
     using Microsoft.CodeAnalysis;
     using Microsoft.CodeAnalysis.CodeActions;
     using Microsoft.CodeAnalysis.CodeFixes;
     using Microsoft.CodeAnalysis.CSharp;
     using Microsoft.CodeAnalysis.CSharp.Syntax;
-    using Microsoft.CodeAnalysis.Editing;
-    using Microsoft.CodeAnalysis.Rename;
     using Microsoft.CodeAnalysis.Text;
-    using System;
-    using System.Collections.Generic;
-    using System.Collections.Immutable;
-    using System.Composition;
-    using System.Linq;
-    using System.Threading;
-    using System.Threading.Tasks;
 
     [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(BraceAnalyzerCodeFixProvider)), Shared]
     public class BraceAnalyzerCodeFixProvider : CodeFixProvider
@@ -24,7 +20,7 @@
         {
             get { return ImmutableArray.Create(BraceAnalyzerAnalyzer.DiagnosticId); }
         }
-        
+
         public sealed override FixAllProvider GetFixAllProvider()
         {
             // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
@@ -35,33 +31,24 @@
         {
             var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
 
-            // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
             var diagnostic = context.Diagnostics.First();
             var diagnosticSpan = diagnostic.Location.SourceSpan;
 
-            // Find the type declaration identified by the diagnostic.
-            var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<TypeDeclarationSyntax>().First();
-            var editor = new SyntaxEditor(root, context.Document.Project.Solution.Workspace);
-            
             // Register a code action that will invoke the fix.
             context.RegisterCodeFix(
                 CodeAction.Create(
                     title: CodeFixResources.CodeFixTitle,
-                    createChangedDocument: c => AddBracesAsync(context.Document, diagnostic, editor),
+                    createChangedDocument: c => AddBracesAsync(root, context.Document, diagnosticSpan),
                     equivalenceKey: nameof(CodeFixResources.CodeFixTitle)),
                 diagnostic);
         }
 
-        Task<Document> AddBracesAsync(Document document, Diagnostic diagnostic, SyntaxEditor editor)
+        Task<Document> AddBracesAsync(SyntaxNode root, Document document, TextSpan diagnosticSpan)
         {
+            // Find the statement to wrap in braces.
+            var statement = root.FindNode(diagnosticSpan).FirstAncestorOrSelf<StatementSyntax>();
             // Compute brace indication.
-            var root = editor.OriginalRoot;
-            var statement = root.FindNode(diagnostic.Location.SourceSpan).FirstAncestorOrSelf<StatementSyntax>();
-            editor.ReplaceNode(statement, (currentStatement, _)
-                => currentStatement.ReplaceNode(currentStatement, SyntaxFactory.Block(currentStatement as StatementSyntax))
-            );
-            var newRoot = editor.GetChangedRoot();
-            return Task.FromResult(document.WithSyntaxRoot(newRoot));
+            return Task.FromResult(document.WithSyntaxRoot(root.ReplaceNode(statement, SyntaxFactory.Block(statement))));
         }
 
     }
